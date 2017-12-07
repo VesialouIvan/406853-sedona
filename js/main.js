8680c50fa3e87d11877e37c4021c877975fce99d
VesialouIvan <vesialouivan@gmail.com>
2017-12-07 18:46:15 +0300

    исправления

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".form-index"); 
+var dayIn = popup.querySelector("[name=date-in]"); 
+popup.classList.add("form-index-hide"), 
+link.addEventListener("click", function(a) { 
+	a.preventDefault(), 
+	popup.classList.toggle("form-index-show"), 
+	dayIn.focus() 
+});
