09bea7a996944920a4723d8fce111d12a996d252
VesialouIvan <vesialouivan@gmail.com>
2017-12-09 12:26:38 +0300

    исправления

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".form-index"); 
 var dayIn = popup.querySelector("[name=date-in]"); 
-popup.classList.add("form-index-hide"), 
+popup.classList.add("form-index"), 
 link.addEventListener("click", function(a) { 
 	a.preventDefault(), 
 	popup.classList.toggle("form-index-show"), 
